aff003f740c5c1a81330a3d1b35527dcc367a96e
Ms-Be <mirimadzudzo@gmail.com>
2024-11-11 22:51:59 +0200

    js update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function refreshWeather(response) {
-  let currenttemperatureElement = document.querySelector("#current-temperature");
+  let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
