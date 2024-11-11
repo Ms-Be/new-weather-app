b2a841df455dbaea64db573a34c7515cb3f10402
Ms-Be <mirimadzudzo@gmail.com>
2024-11-11 20:31:37 +0200

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement,innerHTML=searchInput.value;
-}
+
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
