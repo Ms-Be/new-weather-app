165517c5650797aff277b936c20b65b9ce15bb04
Ms-Be <mirimadzudzo@gmail.com>
2024-11-11 20:16:00 +0200

    updated search engine

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ function displayTemperature(response) {
 
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement,innerHTML=searchInput.value;
+}
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
